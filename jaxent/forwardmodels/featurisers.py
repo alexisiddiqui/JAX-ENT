6e58efce1216c04089b6382a10d7e275e899b5b0
alexisiddiqui <alexisiddiqui831@gmail.com>
2025-01-19 16:50:58 +0000

    some minor changes

--- jaxent/forwardmodels/featurisers.py
+++ jaxent/forwardmodels/featurisers.py
@@ -1,11 +1,47 @@
-from typing import Protocol
+from hdxrate import k_int_from_sequence
 from MDAnalysis import Universe
-from typing import Any
+
+from jaxent.datatypes import HDX_peptide, HDX_protection_factor
+from jaxent.forwardmodels.base import ForwardModel
 
 
-class Featuriser(Protocol):
+class BV_model(ForwardModel):
     """
-    A featuriser is a callable object that takes in a list of universes and then returns a list of features.
+    The BV or Best-Vendruscolo model for HDX-MS data.
+    This computes protection factors using heavy and h bond acceptor (O) contacts.
+    The list of universes must contain compatible residue sequences.
     """
 
-    def __call__(self, ensemble: list[Universe]) -> list[Any]: ...
+    def __init__(self, temperature: float) -> None:
+        super().__init__()
+        self.common_residues: set[tuple[str, int]]
+        self.common_k_ints: list[float]
+        self.compatability = {HDX_peptide, HDX_protection_factor}
+        self.temperature = temperature
+
+    def initialise(self, ensemble: list[Universe]) -> bool:
+        # find the common residue sequence
+
+        residue_sequences = [(u.residues.resnames, u.residues.resids) for u in ensemble]
+        # find the set of common residues in the ensemble
+        common_residues = set.intersection(*[set(r) for r in residue_sequences])
+
+        # find the set of all residues in the ensemble
+        all_residues = set.union(*[set(r) for r in residue_sequences])
+
+        if len(common_residues) == 0:
+            raise ValueError("No common residues found in the ensemble.")
+
+        if common_residues < all_residues:
+            UserWarning("Some residues are not present in all universes. These will be ignored.")
+
+        self.common_residues = common_residues
+
+        self.common_k_ints = [
+            k_int_from_sequence(
+                list(*common_residues),
+                self.temperature,
+            )
+        ]
+
+        return True
