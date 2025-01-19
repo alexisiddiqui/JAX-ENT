1e88b4c69473a6a2b986fba73da87a87a976f332
alexisiddiqui <alexisiddiqui831@gmail.com>
2025-01-19 16:50:58 +0000

    some minor changes

--- jaxent/forwardmodels/base.py
+++ jaxent/forwardmodels/base.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
+from typing import Any, Protocol
+
 from MDAnalysis import Universe
-from jaxent.datatypes import HDX_peptide, HDX_protection_factor
 
 
 class ForwardModel(ABC):
@@ -13,36 +14,9 @@ class ForwardModel(ABC):
         pass
 
 
-class BV_model(ForwardModel):
+class Featuriser(Protocol):
     """
-    The BV or Best-Vendruscolo model for HDX-MS data.
-    This computes protection factors using heavy and h bond acceptor (O) contacts.
-    The list of universes must contain compatible residue sequences.
+    A featuriser is a callable object that takes in a list of universes and then returns a list of features.
     """
 
-    def __init__(self) -> None:
-        super().__init__()
-        self.common_residues: set[tuple[str, int]]
-        self.compatability = {HDX_peptide, HDX_protection_factor}
-
-    def initialise(self, ensemble: list[Universe]) -> bool:
-        # find the common residue sequence
-
-        residue_sequences = [(u.residues.resnames, u.residues.resids) for u in ensemble]
-        # find the set of common residues in the ensemble
-        common_residues = set.intersection(*[set(r) for r in residue_sequences])
-
-        # find the set of all residues in the ensemble
-        all_residues = set.union(*[set(r) for r in residue_sequences])
-
-        if len(common_residues) == 0:
-            raise ValueError("No common residues found in the ensemble.")
-
-        if common_residues < all_residues:
-            UserWarning(
-                "Some residues are not present in all universes. These will be ignored."
-            )
-
-        self.common_residues = common_residues
-
-        return True
+    def __call__(self, ensemble: list[Universe]) -> list[Any]: ...
