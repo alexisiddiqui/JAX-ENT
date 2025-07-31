001ac9d1c116e0fb12d1c4c418a45ac804668c19
alexisiddiqui <alexisiddiqui831@gmail.com>
2025-07-31 19:14:03 +0100

    fixed some test failures

---
+++ jaxent/examples/predict_traj/predict_traj.py
@@ -0,0 +1,15 @@
+"""
+This script is used to calculate the uptake for a given trajectory using the bv model.
+It first featurises the trajectory., and then calculates the residue-level uptake using the bv model using the predict method.
+
+Args:
+    - 'topology': The topology path of the trajectory.
+    - 'trajectory': The trajectory path.
+    - 'output': The output path where the results will be saved.
+    - 'name': The prefix given to the files.
+    - 'bv_bc': bv model bc constant.
+    - 'bv_bh: bv model bh constant.
+    - 'timepoints': The timepoints at which the uptake is calculated - if none provided
+
+
+"""
