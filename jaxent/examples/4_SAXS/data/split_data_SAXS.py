79544c5453f5dfebd62660890eee07b2238077e1
alexisiddiqui <alexisiddiqui831@gmail.com>
2026-03-17 23:23:48 +0000

    created initial SAXS structure

---
+++ jaxent/examples/4_SAXS/data/split_data_SAXS.py
@@ -0,0 +1,16 @@
+"""
+This script loads the reference data and splits it into train and validation sets.
+
+Random, Stratified (alternating), stratified-random (Data-Cluster)
+
+Script args
+- SAXS_data: path to the SAXS data 
+- Split_types: Random, Stratified, Stratified-Random (default all)
+- output_dir: directory to save the split data
+
+Example outputs can be found in jaxent/examples/4_SAXS/simple_validation/_datasplits.
+
+
+
+
+"""
